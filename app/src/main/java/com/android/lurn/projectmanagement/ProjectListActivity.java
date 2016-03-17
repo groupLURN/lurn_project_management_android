0fa1424f24e6a59623e700d5f7aeaba909dfc3e3
Xegara <emmettyoung92@gmail.com>
2016-03-17 17:41:59 +0800

    Integrated RestClient and SwipeRefreshLayout to ProjectListActivity

--- app/src/main/java/com/android/lurn/projectmanagement/ProjectListActivity.java
+++ app/src/main/java/com/android/lurn/projectmanagement/ProjectListActivity.java
@@ -3,6 +3,7 @@ package com.android.lurn.projectmanagement;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,7 +11,12 @@ import android.widget.ListView;
 
 import com.android.lurn.projectmanagement.Models.Adapters.JSONObjectWrapper;
 import com.android.lurn.projectmanagement.Models.Adapters.ProjectsAdapter;
+import com.android.lurn.projectmanagement.Models.Clients.RestClient;
+import com.android.lurn.projectmanagement.Models.Events.PostFailureEvent;
+import com.android.lurn.projectmanagement.Models.Events.PostSuccessEvent;
+import com.squareup.otto.Subscribe;
 
+import java.net.ConnectException;
 import java.util.ArrayList;
 
 /**
@@ -21,7 +27,8 @@ import java.util.ArrayList;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class ProjectListActivity extends BaseActivity implements AdapterView.OnItemClickListener
+public class ProjectListActivity extends BaseActivity implements AdapterView.OnItemClickListener,
+        SwipeRefreshLayout.OnRefreshListener
 {
     private static final String TAG = "ProjectListActivity";
 
@@ -48,11 +55,14 @@ public class ProjectListActivity extends BaseActivity implements AdapterView.OnI
     {
         super.onCreate(savedInstanceState);
 
+        Log.d(TAG, "onCreate() called");
         initializeList();
 
         mListView = (ListView) findViewById(R.id.project_list);
         mListView.setAdapter(new ProjectsAdapter((Context) this, mList));
         mListView.setOnItemClickListener(this);
+
+        mSwipeRefreshLayout.setOnRefreshListener(this);
     }
 
     @Override
@@ -63,5 +73,29 @@ public class ProjectListActivity extends BaseActivity implements AdapterView.OnI
         intent.putExtra(ProjectDetailFragment.ARG_ITEM_ID, Long.toString(mList.get(position).getLong("id")));
         Log.d(TAG, Long.toString(mList.get(position).getLong("id")));
         context.startActivity(intent);
+    }
+
+    @Override
+    public void onRefresh()
+    {
+        Log.d(TAG, "onRefresh() called");
+        new RestClient().execute();
+    }
+
+    @Subscribe
+    public void onPostSuccess(PostSuccessEvent event)
+    {
+        Log.i(TAG, event.getResult().toString());
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Subscribe
+    public void onPostFailure(PostFailureEvent event)
+    {
+        Exception exception = (Exception) event.getResult();
+        if(exception instanceof ConnectException)
+            Log.e(TAG, "Failed to connect. Please make sure the server IP address is " +
+                    "correct or your network connectivity!");
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 }
