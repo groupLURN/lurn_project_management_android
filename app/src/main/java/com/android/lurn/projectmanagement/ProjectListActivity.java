3965246808be7339e511308970701bc727521b73
Xegara <emmettyoung92@gmail.com>
2016-03-18 14:14:16 +0800

    Refactored a lil' bit on ProjectListActivity

--- app/src/main/java/com/android/lurn/projectmanagement/ProjectListActivity.java
+++ app/src/main/java/com/android/lurn/projectmanagement/ProjectListActivity.java
@@ -1,6 +1,5 @@
 package com.android.lurn.projectmanagement;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
@@ -20,19 +19,12 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-/**
- * An activity representing a list of Projects. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link ProjectDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
 public class ProjectListActivity extends MasterActivity implements AdapterView.OnItemClickListener,
         SwipeRefreshLayout.OnRefreshListener
 {
+    public static final String CONTROLLER = "Projects";
+
     private static final String TAG = "ProjectListActivity";
-    private static final String CONTROLLER = "Projects";
 
     private ArrayList<JSONObject> mList = new ArrayList<JSONObject>();
     private ListView mListView;
@@ -59,11 +51,11 @@ public class ProjectListActivity extends MasterActivity implements AdapterView.O
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id)
     {
-        Context context = (Context) this;
-        Intent intent = new Intent(context, ProjectDetailActivity.class);
         Log.d(TAG, "Sending id:" + Long.toString(id) + " to detail activity.");
-        intent.putExtra(ProjectDetailFragment.ARG_ITEM_ID, Long.toString(id));
-        context.startActivity(intent);
+
+        Intent intent = new Intent(this, ProjectDetailActivity.class);
+        intent.putExtra(ProjectDetailActivity.ARG_ITEM_ID, Long.toString(id));
+        startActivity(intent);
     }
 
     @Override
